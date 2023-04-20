2fa7dc28d4a95b5584e7f4f96b0c4b3811e2faf3
maks <maks.ovsienko2@gmail.com>
2023-04-20 15:23:34 +0200

    sth changed

---
+++ src/main/java/org/example/snyk/Credentials.java
@@ -0,0 +1,26 @@
+package org.example.snyk;
+
+public class Credentials {
+    final static String pr_key = "-----BEGIN RSA PRIVATE KEY-----\n" +
+            "MIICXQIBAAKBgHxpCqgndvrzo4vfMJB6ciG2nTiz9mlAX0QKorUi1mPc+YaLwRl0\n" +
+            "Pm2nehaBGZXt8z2jqPCrQ6Yhpran0k5jEz4Da7jFnRZnaUcGrys5KFgGasQAx3XK\n" +
+            "DkcFizE/6lnXCDpY8sfWXCy3JDM2c7A0z8wVtc8H7MFmslAcCfowWTx9AgMBAAEC\n" +
+            "gYAWywvnyOs0iQSysJ5uKfDVGmbtp5MCr4eEqMosk4hh3W9d5ibnqfNyFZf5nYd5\n" +
+            "f5Hr/hsKlOYc/tdTaaXPNLmmhTe4YJazRqqbPsgAtN0Psk9HHbZt8y7pGANIXr3n\n" +
+            "V2JeFD8qUKzkKw2cvJRHjPpH0cjWOOnCaBVski9EiLAuQQJBANnzH1UCRnerjIPE\n" +
+            "283GM7U2katRTSSkX3LU8E6dgVZg0KYzhu0HS3tJ91Cp/HmNFj9srcZDUBo2YOm2\n" +
+            "avj4RtECQQCSIVdx8kCLr0ZeQmXUoNWthh+tohZGmkaaGi0y64kOBIhdq+dNmW4J\n" +
+            "xj1M+w6jZ/ZU1R9toD8XZhEFFj8dWh3tAkEAmGD+uvSISArMhyQPeWLlrACm/9wr\n" +
+            "zz6M7UArcwtz7exqdIj6AKZlA6HHI8J1Hpahw148cK2HR4xVXZGIYOFUwQJBAI1h\n" +
+            "gsg02KwM7ake1oTtAy9618VXT/QJKYvJDbf5WEXerszqF8TlUChWy1JiiFwippqX\n" +
+            "BGw9IS2TS54/UtskKFUCQQDQuhQOR6nOpEwlVdj40PNLgFJxvrSsNAmql5jH6FCs\n" +
+            "HJjMhFoDU23FU5FpDD4lQ92b6ilprVPqbYRPRemqTeVV\n" +
+            "-----END RSA PRIVATE KEY-----";
+    final static String public_key = "-----BEGIN PUBLIC KEY-----\n" +
+            "MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgHxpCqgndvrzo4vfMJB6ciG2nTiz\n" +
+            "9mlAX0QKorUi1mPc+YaLwRl0Pm2nehaBGZXt8z2jqPCrQ6Yhpran0k5jEz4Da7jF\n" +
+            "nRZnaUcGrys5KFgGasQAx3XKDkcFizE/6lnXCDpY8sfWXCy3JDM2c7A0z8wVtc8H\n" +
+            "7MFmslAcCfowWTx9AgMBAAE=\n" +
+            "-----END PUBLIC KEY-----";
+
+}
